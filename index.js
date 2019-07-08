4e31e6fb4464c376a9c4efe40b9ab918c56de51e
Rüya Rava Rajab <rawaa.rajab@gmail.com>
2019-07-08 07:15:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Write your code in this file!
 
-const currentUser = 'Rawaa Rajab';
+const currentUser = 'Grace Hopper';
 
-const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = 'Welcome, ' + currentUser.split(' ')[0] + '!';
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
